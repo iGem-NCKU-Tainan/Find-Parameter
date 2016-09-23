7670c3e17adcfe3a51f2ae4f9e438bf4157336e7
jingfei <jingfei955047@gmail.com>
2016-09-23 17:54:54 +0800

    Newer version.

--- Main.m
+++ Main.m
@@ -1,18 +1,27 @@
+
 clear all
 close all
 clc
 
-number=6;  %number of data set
-file = './data/Repeat1.txt';
+%輸進10筆資料
+number=10;
+%數據的時間間隔是三分鐘
+t=3;
+%這邊檔名改成 ''現在時間/用戶姓名''
+outputName='result.txt';
+file = '/Users/chenshuyu/Desktop/Find Parameter/0822exp/D.csv';
 %load in data set
-concentration=[0,0,0.1,0.5,1,5];
 S=load(file);
+
+
 OD_time=S(:,1);
+%把原始螢光讀值輸入
 for i=1:number
     OD(:,i)=S(:,i+1)
 end
 
 %calculate speed
+%兩兩相減計算時間變化率
 for j = 1:number
     for i= 1:(length(OD_time)-1)
     Speed_time(i)=OD_time(i+1);
@@ -20,27 +29,64 @@ for j = 1:number
     end
 end
 
-%Run average every 50
+
+
+%Run average every 25
 for k = 1:number
-   for i= 1:(length(Speed_time)-50)
+   for i= 1:(length(Speed_time)-25)
     avg_time(i)=Speed_time(i);
     sum=0;
-    for j=0:(50-1)
+    for j=0:(25-1)
         sum=sum+Speed(i+j,k);
     end
-    avg(i,k)=sum/50;
+    avg(i,k)=sum/25;
     end
 end
-
-% clear sum to avoid overloading built-in function
-clear sum
-
-
-%split it AND plot
+%Large counting number
+l=0;
+%R counting number
+m=1;
+%對十次實驗值做分析以及畫圖
 for k=1:number
-    subplot(2,number/2,k);
-    plot(avg_time,avg(:,k));
-    xlabel('time(mins)');
-    ylabel('speed');
-    title(concentration(k));
+    for i=1:length(avg_time);
+        if avg(i,k)==max(avg(:,k));
+            t0(k)=avg_time(i);
+        end
+    end
+    Vmax=max(avg(:,k))/3;
+    Vmax_k(k)=Vmax;
+    %這個判定值有可能會改
+    if Vmax >= 4.6
+        l=l+1;
+        x=-1;
+    else
+    %這個函式有可能會改
+    x=(18.6542*Vmax-38.2493)/(5.07-Vmax);
+    end
+    if x<=0
+
+        continue;
+    else
+        R(m)=x^(1/2.0183);
+        m=m+1;
+    end
+
 end
+
+if m<=number/2
+    if l>number/2
+        statement='The concentration is too high!!'
+    else
+        statement='The concentration is lower than 0.1'
+    end
+
+else
+    %這個直方圖不知道能不能在Ｃ上面畫出來
+    hist(R);
+   M=mean(R);
+   S=std(R)
+   %不知道怎麼讓他跑出來
+    statement=['The mean value is ' ,M,'Standard deviation is',S]
+end
+%Use print(statement) here
+statement;
